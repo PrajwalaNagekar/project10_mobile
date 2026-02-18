1cf513467dc47c935416f663b19e9f77ee161157
PrajwalaNagekar <prajwalanagekar@gmail.com>
2026-02-18 20:19:12 +0530

    small fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import  { useState, useEffect, useCallback } from "react";
 import {
   BarChart, Bar, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
 } from "recharts";
